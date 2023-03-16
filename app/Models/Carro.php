1e1b6e84725544f99c377f27425b469ea76aa090
Aldejunior <aldejunioroliveira@gmail.com>
2023-03-15 22:29:04 -0300

    metade do projeto

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+    protected $table ='Carros';
+
+    protected $fillable = [
+
+        'modelo',
+        'marca',
+        'cor',
+        'motor',
+        'ano'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/Carros', [CarrosController::class, 'index'])->name('carros.index');
+
+route::get('/Carros', [CarrosContreller::class,'create'])->name('Carros.create');
+
+route::get('/Carros/{id}', [CarrosController::class, 'show'])->name('Carros.show');
+
+route::get('/Carros/{id}/edit', [CarrosController::class, 'edit'])->name('Carros.edit');
+
+route::get('/Carros', [CarrosController::class, 'store'])->name('Carros.store');
+
+route::get('/Carros/{id}', [CarrosController::class, 'update'])->name('Carros.update');
+
+route::get('/Carros/{id}', [CarrosController::class, 'destroy'])->name('Carros.destroy');
